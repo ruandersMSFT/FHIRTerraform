0d35c62d075f95b6ee90b2ea57fe3f40d7680150
ruandersMSFT <russell@russellanderson.net>
2023-10-25 19:48:03 -0600

    .

--- Modules/PrivateEndpoint/main.tf
+++ Modules/PrivateEndpoint/main.tf
@@ -6,20 +6,21 @@ locals {
 }
 
 resource "azurerm_private_endpoint" "this" {
-  name                = var.name
+  name                = "${var.name}-endpoint"
   location            = coalesce(var.location, data.azurerm_resource_group.this.location)
   resource_group_name = var.resource_group_name
   subnet_id           = var.subnet_id
 
   private_service_connection {
-    name                           = "${var.name}-connection"
+    name                           = "${var.name}-endpoint-psc"
     private_connection_resource_id = var.resource_id
     is_manual_connection           = coalesce(var.approval_required, false)
     subresource_names              = var.subresource_names
   }
 
+  #todo russell, ip_configuration at CDC
   private_dns_zone_group {
-    name                 = "example-dns-zone-group" #todo
+    name                 = "example-dns-zone-group"
     private_dns_zone_ids = [var.private_dns_zone_id]
   }
 
